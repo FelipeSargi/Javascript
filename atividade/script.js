20b84f62c4f540977a4773937ed0e20f95298a54
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2024-05-16 22:05:28 -0300

    Atividade DOM

---
+++ atividade/script.js
@@ -0,0 +1,5 @@
+const elemento = document.querySelectorAll("img");
+
+const paragrafo = document.querySelector("#texto");
+paragrafo.insertAdjacentHTML('beforebegin','<p>Belas padarias</p>');
+paragrafo.style.color="red";
